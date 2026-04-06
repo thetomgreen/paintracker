287bddc089ab78981bf6f2e3b565dd1b8b58bb26
Tom Green <tomgreen@Roxys-MacBook-Air.local>
2026-04-06 03:58:26 -0700

    Hardcode dev schema for staging branch

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
-const dbSchema = process.env.NEXT_PUBLIC_DB_SCHEMA || "public";
+const dbSchema = process.env.NEXT_PUBLIC_DB_SCHEMA || "dev";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const supabase: any = supabaseUrl
